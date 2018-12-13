3a8c55e04fdb1a97bfa96e5fe88fc8db053432b1
zmg02 <1225798710@qq.com>
2018-12-13 10:03:44 +0000

    Finish login and logout

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -29,13 +29,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * a simple convenience so we don't have to attach every token manually.
  */
 
-let token = document.head.querySelector('meta[name="csrf-token"]');
+// Laravel 默认创建了一个 JavaScript 变量，
+// 此变量会从页面的元标签中取 CSRF 密钥，取不到就会报错。
+// 本教程中我们不需要使用到该变量，把以上代码删除即可。
+// let token = document.head.querySelector('meta[name="csrf-token"]');
 
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+// if (token) {
+//     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+// } else {
+//     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+// }
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
