3d81f5f65822aa56e3bb3e4fc22282315aed7938
zmg02 <1225798710@qq.com>
2018-12-17 06:17:54 +0000

    Add account activations

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,4 +1,4 @@
-@foreach (['danger', 'warning', 'sucsess', 'info'] as $msg)
+@foreach (['danger', 'warning', 'success', 'info'] as $msg)
     @if(session()->has($msg))
         <div class='flash-message'>
             <p class='alert alert-{{ $msg }}'>
